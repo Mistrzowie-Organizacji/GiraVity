e535b58e9c97c2c7f55aff891880bf18d350be2e
Hessbrees <lenart483@gmail.com>
2022-11-19 18:20:39 +0100

    Main menu buttons behaviours

--- Assets/Scripts/MenuUI.cs
+++ Assets/Scripts/MenuUI.cs
@@ -2,20 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MenuUI : MonoBehaviour
 {
-    void OnClickStart()
+    private GameObject howToPlay;
+
+    private void Awake()
+    {
+        howToPlay =  transform.Find("HowToPlay").gameObject;
+    }
+    public void OnClickStart()
     {
 
     }
 
-    void OnClickHowToPlay()
+    public void OnClickOpenHowToPlay()
     {
+        howToPlay.SetActive(true);
 
     }
-
-    void OnClickExit()
+    public void OnClickCloseHowToPlay()
     {
+        howToPlay.SetActive(false);
 
+    }
+    public void OnClickExit()
+    {
+        Application.Quit();
     }
 }
