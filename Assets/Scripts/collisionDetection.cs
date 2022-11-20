aa29c2e863cc5d097cfb059ac046ee4b419b2a60
davidkuc <davidkuc2@onet.pl>
2022-11-20 04:26:44 +0100

    Optimized core a bit more

--- Assets/Scripts/collisionDetection.cs
+++ Assets/Scripts/collisionDetection.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,26 +7,26 @@ public class collisionDetection : MonoBehaviour
     public int HP = 3;
     void Start()
     {
-        
+
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.ToString() == "enemy_bullet(Clone) (UnityEngine.GameObject)")
-       {
+        if (collision.gameObject.ToString() == "enemy_bullet(Clone) (UnityEngine.GameObject)")
+        {
             HP -= 1;
             Destroy(collision.gameObject);
-       }
-       if (HP == 0)
-       {
+        }
+        if (HP == 0)
+        {
             SceneManager.LoadScene("GameOver");
-       }
-        
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
