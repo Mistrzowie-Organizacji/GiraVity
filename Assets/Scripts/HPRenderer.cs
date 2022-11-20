a448716ef4dfaba0cdea2584c566dcb4d9066f6f
davidkuc <davidkuc2@onet.pl>
2022-11-20 04:10:06 +0100

    OPtimized code

--- Assets/Scripts/HPRenderer.cs
+++ Assets/Scripts/HPRenderer.cs
@@ -1,37 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class HPRenderer : MonoBehaviour
 {
+    private collisionDetection collisionDetection;
+    private HPIcon hpIcon;
+
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < 3; i++)
+        hpIcon = GameObject.Find("HPIcon").GetComponent<HPIcon>();
+        for (int i = 0; i < 3; i++)
         {
-            GameObject icon = GameObject.Instantiate(GameObject.Find("HPIcon"), new Vector3(0+i*50, 0, 0), Quaternion.identity);
-            
+            GameObject icon = GameObject.Instantiate(hpIcon.gameObject, new Vector3(0 + i * 50, 0, 0), Quaternion.identity);
+
             icon.transform.SetParent(GameObject.Find("Canvas").transform);
             icon.name = "HPIcon" + i;
         }
-        GameObject.Find("HPIcon").SetActive(false);
+        hpIcon.gameObject.SetActive(false);
+
+        collisionDetection = GameObject.Find("giraffe").GetComponent<collisionDetection>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        int HP = GameObject.Find("giraffe").GetComponent<collisionDetection>().HP;
-        for(int i=0; i<3; i++)
+        for (int i = 0; i < 3; i++)
         {
-            if(i+1 <= HP)
+            if (i + 1 <= collisionDetection.HP)
             {
-                GameObject.Find("HPIcon" + i).GetComponent<HPIcon>().active = true;
+               hpIcon.active = true;
             }
             else
             {
-                GameObject.Find("HPIcon" + i).GetComponent<HPIcon>().active = false;
+                hpIcon.active = false;
             }
         }
-        
+
     }
 }
