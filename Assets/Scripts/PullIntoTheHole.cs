424e7db398616d2dec66534ff295f1fc2d50bc3e
DawidMiner <dawidgornikowski@o2.pl>
2022-11-20 03:55:11 +0100

    Animation and pullin for black hole

--- Assets/Scripts/PullIntoTheHole.cs
+++ Assets/Scripts/PullIntoTheHole.cs
@@ -10,29 +10,49 @@ public class PullIntoTheHole : MonoBehaviour
     private GameObject player;
     private Vector2 direct;
     private float min_distance = 7f;
-
+    private int easingIDx = -1;
+    private int easingIDy = -1;
     // Start is called before the first frame update
     void Start()
     {
         blackHole = GameObject.Find("black_hole_official");
         player = GameObject.Find("giraffe");
+        StartCoroutine(checkColission());
     }
-
+    IEnumerator checkColission()
+    {
+        while (true)
+        {
+            check_collision();
+            yield return null;
+        }
+    }
     private void check_collision()
     {
-        min_distance += 0.01f;
+        min_distance += 0.001f;
         distance = Vector3.Distance(blackHole.transform.position, player.transform.position);
         if (distance < min_distance)
         {
-            print(min_distance);
-            player.transform.position = blackHole.transform.position - (blackHole.transform.position - player.transform.position) * 0.98f;
+            print(distance);
+
+            //player.transform.position = blackHole.transform.position + (player.transform.position - blackHole.transform.position) * 0.97f;
+            if (!LeanTween.isTweening(easingIDx))
+                easingIDx = LeanTween.value(player.transform.position.x, blackHole.transform.position.x + (player.transform.position.x - blackHole.transform.position.x) * 0.85f, 0.1f).setOnUpdate(IncreaseValue).uniqueId;
+            if (!LeanTween.isTweening(easingIDy))
+                easingIDy = LeanTween.value(player.transform.position.y, blackHole.transform.position.y + (player.transform.position.y - blackHole.transform.position.y) * 0.85f, 0.1f).setOnUpdate(IncreaseValueY).uniqueId;
         }
 
     }
     // Update is called once per frame
-    void Update()
+    private void IncreaseValue(float valueX)
     {
-        check_collision();
+        player.transform.position = new Vector3(valueX, player.transform.position.y);
+    }
+
+    private void IncreaseValueY(float valueY)
+    {
+        player.transform.position = new Vector3(player.transform.position.x, valueY);
     }
 }
+
 
